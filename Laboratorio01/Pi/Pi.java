38065426866d1840b25d42a77aa30fca0a11c3c5
Luis Flores <lfloresh@uni.pe>
2020-06-15 23:22:09 -0500

    Agregando Pi

---
+++ Laboratorio01/Pi/Pi.java
@@ -0,0 +1,55 @@
+
+
+import java.util.*;
+import java.math.*;
+public class Pi {
+	public static Random random = new Random();
+	
+	public static void main (String[] args) throws InterruptedException {
+		BigDecimal N = BigDecimal.valueOf(1000);  
+		int RUNS = 20;   // veces que crecera la muestra
+		BigDecimal CONST = BigDecimal.valueOf(2);    // constante de crecimiento 
+		long start,end;
+		for (int i = 1; i <= RUNS; i++) {
+			// medicion del tiempo de ejecucion
+			start = System.currentTimeMillis();
+			BigDecimal c = parallelPi(N);
+			end = System.currentTimeMillis();
+			//double total = N;
+
+			BigDecimal result =  c.divide(N,MathContext.DECIMAL128);
+			
+			System.out.printf("%d) "+N+" experimentos :  %6d ms  "+result+"\n", i, end - start);
+			N = N.multiply(CONST,MathContext.DECIMAL128);   // crecimiento de la muestra
+		}
+	
+	}
+	public static BigDecimal parallelPi(BigDecimal N) {
+		BigDecimal threads = new BigDecimal("4");
+		return parallelPi(N, threads);
+	}
+	
+	public static BigDecimal parallelPi(BigDecimal N, BigDecimal threadCount) {
+		BigDecimal sum = BigDecimal.ZERO;
+		BigDecimal width = BigDecimal.ONE.divide(N,MathContext.DECIMAL128);
+		
+		Calc mythread[] = new Calc[threadCount.intValue()];
+		
+
+		for(BigDecimal i = BigDecimal.ZERO; i.compareTo(threadCount) < 0; i = i.add(BigDecimal.ONE)) {
+			BigDecimal start = (i.multiply(N, MathContext.DECIMAL128)).divide(threadCount,MathContext.DECIMAL128);
+			BigDecimal end = ((i.add(BigDecimal.ONE)).multiply(N,MathContext.DECIMAL128)).divide(threadCount,MathContext.DECIMAL128);
+			mythread[i.intValue()] = new Calc(start, end, width);
+			mythread[i.intValue()].start();
+		}
+		
+		
+		for(int j = 0; j < threadCount.intValue(); j++) {
+			try{ 
+				mythread[j].join();
+				sum = sum.add(mythread[j].getSum());
+			}catch(Exception e){System.out.println(e);} 
+		}			
+		return sum;
+	}
+}
---
+++ Laboratorio01/Pi/PiSec.java
@@ -0,0 +1,32 @@
+import java.math.*;
+public class PiSec {
+	public static void main(String[] args) {
+		BigDecimal N = BigDecimal.valueOf(1000);
+		int RUNS = 20;
+		BigDecimal CONST = BigDecimal.valueOf(2);
+		long start,end;
+		for (int i = 1; i <= RUNS; i++) {
+			// medicion del tiempo de ejecucion
+			start = System.currentTimeMillis();
+			BigDecimal c = secuantialPi(N);
+			end = System.currentTimeMillis();
+
+			BigDecimal result =  c.divide(N,MathContext.DECIMAL128);
+			
+			System.out.printf("%d) "+N+" experimentos :  %6d ms  "+result+"\n", i, end - start);
+			N = N.multiply(CONST,MathContext.DECIMAL128);   // crecimiento de la muestra
+		}
+	}
+	public static BigDecimal secuantialPi(BigDecimal N) {
+		BigDecimal sum = BigDecimal.ZERO;
+		BigDecimal width = BigDecimal.ONE.divide(N,MathContext.DECIMAL128);
+		BigDecimal mid, height;
+		for(BigDecimal i= BigDecimal.ZERO; i.compareTo(N)<0;i = i.add(BigDecimal.ONE)) {
+			mid = (i.add(BigDecimal.valueOf(0.5))).multiply(width,MathContext.DECIMAL128);
+            height = BigDecimal.valueOf(4.0).divide(BigDecimal.ONE.add(mid.multiply(mid,MathContext.DECIMAL128)),MathContext.DECIMAL128);
+            sum = sum.add(height);
+		}
+		return sum;
+	}
+
+}
