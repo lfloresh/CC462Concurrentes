31ff0f77afd5817062a111926ea660f39d7a33b5
Luis Flores <lfloresh@uni.pe>
2020-06-17 01:35:55 -0500

    final

--- Laboratorio01/MergeSort/Sorter.java
+++ Laboratorio01/MergeSort/Sorter.java
@@ -1,12 +1,12 @@
 public class Sorter implements Runnable {
 	private float[] a;
 	private int threadCount;
-	
+
 	public Sorter(float[] a, int threadCount) {
 		this.a = a;
 		this.threadCount = threadCount;
 	}
-	
+
 	public void run() {
 		MergeSort.parallelMergeSort(a, threadCount);
 	}
--- Laboratorio01/Pi/Calc.java
+++ Laboratorio01/Pi/Calc.java
@@ -1,31 +1,34 @@
 
 import java.math.*;
+
 public class Calc extends Thread {
 
-	private final BigDecimal start;
+    private final BigDecimal start;
     private final BigDecimal end;
     private final BigDecimal width;
 
     private BigDecimal sum;
-    
+
     public Calc(BigDecimal start, BigDecimal end, BigDecimal width) {
         this.start = start;
         this.end = end;
         this.sum = BigDecimal.ZERO;
         this.width = width;
     }
-	
+
     @Override
-	public void run() {
-    	BigDecimal mid, height;
+    public void run() {
+        BigDecimal mid, height;
 
         for (BigDecimal i = start; i.compareTo(end) < 0; i = i.add(BigDecimal.ONE)) {
-            mid = (i.add(BigDecimal.valueOf(0.5))).multiply(width,MathContext.DECIMAL128);
-            height = BigDecimal.valueOf(4.0).divide(BigDecimal.ONE.add(mid.multiply(mid,MathContext.DECIMAL128)),MathContext.DECIMAL128);
+            mid = (i.add(BigDecimal.valueOf(0.5))).multiply(width, MathContext.DECIMAL128);
+            height = BigDecimal.valueOf(4.0).divide(
+                    BigDecimal.ONE.add(mid.multiply(mid, MathContext.DECIMAL128)),
+                    MathContext.DECIMAL128);
             this.sum = this.sum.add(height);
         }
-	}
-	
+    }
+
     public BigDecimal getSum() {
         return sum;
     }
